a61ef5407df53efc12fcbcbb95ac3374b1e903a1
Alex Schenck <AlexNSchenck@gmail.com>
2014-12-05 15:02:47 -0800

    Style
    Bolded headings, added space between collapse items, set all panels
    collapsed by default

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+$('document').ready($('.panel-collapse').collapse('hide'));
+
 angular.module("BlogApp", [])
 	.controller("BlogController", function($scope) {
 		$scope.posts = posts;
